3a28c2c338d65a63e7eb52d0c55dd57e84ae75a8
Matthew Stewart <mattdste@gmail.com>
2014-09-10 18:18:59 -0400

    hopefully email error handling works with gmail

--- config.py
+++ config.py
@@ -1,7 +1,43 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+class TlsSMTPHandler(logging.handlers.SMTPHandler):
+    def emit(self, record):
+        """
+        Emit a record.
+ 
+        Format the record and send it to the specified addressees.
+        """
+        try:
+            import smtplib
+            import string # for tls add this line
+            try:
+                from email.utils import formatdate
+            except ImportError:
+                formatdate = self.date_time
+            port = self.mailport
+            if not port:
+                port = smtplib.SMTP_PORT
+            smtp = smtplib.SMTP(self.mailhost, port)
+            msg = self.format(record)
+            msg = "From: %s\r\nTo: %s\r\nSubject: %s\r\nDate: %s\r\n\r\n%s" % (
+                            self.fromaddr,
+                            string.join(self.toaddrs, ","),
+                            self.getSubject(record),
+                            formatdate(), msg)
+            if self.username:
+                smtp.ehlo() # for tls add this line
+                smtp.starttls() # for tls add this line
+                smtp.ehlo() # for tls add this line
+                smtp.login(self.username, self.password)
+            smtp.sendmail(self.fromaddr, self.toaddrs, msg)
+            smtp.quit()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
+ 
+ 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
@@ -51,11 +87,11 @@ class ProductionConfig(Config):
 		import logging
 		from logging.handlers import SMTPHandler
 
-		ADMINS = [os.environ.get(ADMIN)]
-		mail_handler = SMTPHandler(os.environ.get(MAIL_SERVER),
-								   os.environ.get(MAIL_USERNAME),
+		ADMINS = [os.environ.get('ADMIN')]
+		mail_handler = TlsSMTPHandler(os.environ.get('MAIL_SERVER'),
+								   os.environ.get('MAIL_USERNAME'),
 								   ADMINS, 'YourApplication Failed',
-								   credentials=(os.environ.get(MAIL_USERNAME), os.environ.get(MAIL_PASSWORD)))
+								   credentials=(os.environ.get('MAIL_USERNAME'), os.environ.get('MAIL_PASSWORD')))
 		mail_handler.setLevel(logging.ERROR)
 		app.logger.addHandler(mail_handler)
 
