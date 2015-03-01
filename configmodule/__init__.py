84f9e76f330c759e325d27308f1e500cf2fedd87
Matthew Stewart <mattdste@gmail.com>
2015-03-01 02:50:34 -0600

    fixed email config settings

--- configmodule/__init__.py
+++ configmodule/__init__.py
@@ -9,8 +9,9 @@ class Config(object):
 	SSL_DISABLE = False
 	SQLALCHEMY_RECORD_QUERIES = True	
 	MAIL_SERVER = os.environ.get('MAIL_SERVER')
-	MAIL_PORT = os.environ.get('MAIL_PORT')
-	MAIL_USE_SSL = os.environ.get('MAIL_USE_TLS')
+	MAIL_PORT = os.environ.get('MAIL_PORT', 443)
+	MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', False)
+	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', True)
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	MAIL_SUBJECT_PREFIX = '[Michigan Hackers]'
