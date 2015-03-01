cc25863d20ca0cfadaa984bdb13ae375f8d379d4
Matthew Stewart <mattdste@gmail.com>
2015-03-01 01:50:13 -0600

    reenabled mail handling

--- configmodule/__init__.py
+++ configmodule/__init__.py
@@ -8,11 +8,11 @@ class Config(object):
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 	SSL_DISABLE = False
 	SQLALCHEMY_RECORD_QUERIES = True	
-	# MAIL_SERVER = os.environ.get('MAIL_SERVER')
-	# MAIL_PORT = os.environ.get('MAIL_PORT')
-	# MAIL_USE_SSL = os.environ.get('MAIL_USE_TLS')
-	# MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-	# MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = os.environ.get('MAIL_PORT')
+	MAIL_USE_SSL = os.environ.get('MAIL_USE_TLS')
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	MAIL_SUBJECT_PREFIX = '[Michigan Hackers]'
 	MAIL_SENDER = 'Michigan Hackers <michiganhackers@umich.edu>'
 	
