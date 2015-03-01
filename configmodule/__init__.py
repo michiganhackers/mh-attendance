169ece7f89eb6def895481124a1608737256966a
Matthew Stewart <mattdste@gmail.com>
2015-03-01 03:05:57 -0600

    config email booleans were needed to be converted from strings

--- configmodule/__init__.py
+++ configmodule/__init__.py
@@ -1,4 +1,5 @@
 import os, logging
+from distutils.util import strtobool
 
 basedir = os.path.abspath(__file__ + "/../../")
 print basedir
@@ -10,8 +11,8 @@ class Config(object):
 	SQLALCHEMY_RECORD_QUERIES = True	
 	MAIL_SERVER = os.environ.get('MAIL_SERVER')
 	MAIL_PORT = os.environ.get('MAIL_PORT', 465)
-	MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', False)
-	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', True)
+	MAIL_USE_SSL = bool(strtobool(os.environ.get('MAIL_USE_SSL')))
+	MAIL_USE_TLS = bool(strtobool(os.environ.get('MAIL_USE_TLS')))
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	MAIL_SUBJECT_PREFIX = '[Michigan Hackers]'
